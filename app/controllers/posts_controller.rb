3190da99c3b7c90ddcf5b7a24fe8346e71671dab
PankajBehera-1 <pankajbehera752@gmail.com>
2023-08-20 19:21:44 +0530

    avatar

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
     before_action :authenticate_user!, except: [:index, :show]
   
     def index
-      @posts = Post.includes(:user).all
+      @posts = Post.includes(user: :avatar).all
     end
   
     def show
